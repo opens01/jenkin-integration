1595ca722edbb73e9be6a1d0632f687401e391d6
Rishi <openinggs01@gmail.com>
2026-08-13 09:02:54 +0530

    changes

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
 public class sample{
-    public static void Main(string args[]){
-        system.out.print("hello world");
+    public static void main(String args[]){
+        System.out.print("hello world");
 }
 }
